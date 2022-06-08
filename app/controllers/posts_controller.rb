89b5691e675646553e783343aa2012f62d2e7dca
adri48 <adriana.gomez@kellogg.northwestern.edu>
2022-06-08 13:14:30 +0000

    finalupdates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
       redirect_to "/login"
     else
       @post["title"] = params["post"]["title"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
       @post["description"] = params["post"]["description"]
       @post["posted_on"] = params["post"]["posted_on"]
       @post["place_id"] = params["post"]["place_id"]
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    class Post < ApplicationRecord
+        has_one_attached :uploaded_image
+      end
+ <div class="mb-3">
+<%= form.label "uploaded_image", :class => "form-label" %>
+<%= form.file_field "uploaded_image", :accept => "image/png,image/jpg,image/jpeg,image/gif", :class => "form-control" %>
+</div>
 end
