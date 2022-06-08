28354c1a67428e6a0c003ef9d1340e6f6c369a00
adri48 <95722862+adri48@users.noreply.github.com>
2022-06-07 20:36:30 -0400

    Don't let new users with empty usernames be saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["username"] = params["user"]["username"]
-    @user["email"] = params["user"]["email"]
-    @user["password"] = BCrypt::Password.create(params["user"]["password"])
-    @user.save
-    redirect_to "/users/#{@user["id"]}"
+    if params["user"]["username"].empty? ||  params["user"]["email"].empty? ||  params["user"]["password"].empty?
+      flash["notice"] = "Please enter values for all fields."
+      redirect_to "/users/new"
+    else
+      @user = User.new
+      @user["username"] = params["user"]["username"]
+      @user["email"] = params["user"]["email"]
+      @user["password"] = BCrypt::Password.create(params["user"]["password"])
+      @user.save
+      session["user_id"] = @user["id"]
+      redirect_to "/places"
+    end
   end
 end
