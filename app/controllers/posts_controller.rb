5b7d9b1ae2fc82a2914e5c79ec2be1b1cc03504a
adri48 <95722862+adri48@users.noreply.github.com>
2022-06-07 20:48:48 -0400

    Add new posts for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,18 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+    if session["user_id"].nil?
+      flash["notice"] = "Please sign in before creating a post."
+      redirect_to "/login"
+    else
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = session["user_id"]
+      @post.save
+      redirect_to "/places/#{@post["place_id"]}"
+    end
   end
 
 end
